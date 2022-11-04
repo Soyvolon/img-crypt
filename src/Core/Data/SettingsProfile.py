faf28b4ae940035e49cfe8117fef752d72ea77e2
Bounds <andybounds@gmail.com>
2022-11-04 19:42:43 -0400

    Started work on the image mod service.

--- src/Core/Data/SettingsProfile.py
+++ src/Core/Data/SettingsProfile.py
@@ -1,6 +1,7 @@
 # Last Edit: 2022-10-28
 # Author(s): Bounds, Hayden
 
+from typing import List
 from uuid import UUID
 from .UserProfile import UserProfile
 
@@ -16,3 +17,16 @@ class SettingsProfile():
         self.pixelSpacing = pixelSpacing
         self.colorSettings = colorSettings
         self.encryptKey = encryptKey
+
+    def get_header(self) -> List[int]:
+        # set a true flag if there is an encrypt key ...
+        hasEncrypt = int(self.encryptKey and not self.encryptKey.isspace())
+        # ... then return the header as a set of integers.
+        return [self.charPerPixel, 
+            self.pixelSpacing, 
+            self.colorSettings, 
+            hasEncrypt]
+
+    @staticmethod
+    def build_settings(headerPixels: List[(int, int, int)], encryptKey: str = None):
+        raise NotImplementedError()
--- src/Core/Services/ImageModificationService.py
+++ src/Core/Services/ImageModificationService.py
@@ -1,7 +1,10 @@
 # Last Edit: 2022-10-28
 # Author(s): Bounds
 
-from typing import Tuple
+from PIL import Image
+import numpy
+
+from typing import List, Tuple
 from .ImageModificationServiceInterface import ImageModificationServiceInterface as IMSI
 from ..Data import SettingsProfile
 
@@ -10,7 +13,34 @@ class ImageModificationService(IMSI):
         pass
 
     def hide_text_in_image(self, profile: SettingsProfile, text: str, inputPath: str, outputPath: str) -> None:
-        return super().hide_text_in_image(profile, text, inputPath, outputPath)
+        # lets do some input validation
+        if not profile:
+            raise ValueError(profile)
+        if not text:
+            text = ''
+        if not inputPath:
+            raise ValueError(inputPath)
+        if not outputPath:
+            raise ValueError(outputPath)
+
+        # lets process some images!
+        # ... start by loading the image into
+        # memory in a way we can get the pixels ...
+        with Image.open(inputPath) as source:
+            # ... get the size of the image ...
+            w, h = source.size
+            # ... and its pixels ...
+            pixels = list(source.getdata())
+            # ... then the image mode ...
+            if source.mode == "RGBA":
+                channels = 4
+            elif source.mode == "RGB":
+                channels = 3
+            # ... then get a numpy array of the pixel values ...
+            pixel_values = numpy.array(pixels).reshape((h, w, channels))
+
+            
+
 
     def reveal_text_in_image(self, inputPath: str, encryptKey: str = "") -> Tuple[SettingsProfile, str]:
         return super().reveal_text_in_image(inputPath, encryptKey)
