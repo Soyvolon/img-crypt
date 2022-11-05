8a209274a3423611c7b6286da77d49bb0a850052
ChopstickLeg <72234304+ChopstickLeg@users.noreply.github.com>
2022-11-04 22:15:33 -0400

    Added database connection

--- src/Core/Services/ProfileManagementService.py
+++ src/Core/Services/ProfileManagementService.py
@@ -1,21 +1,35 @@
-# Last Edit: 2022-10-28
-# Author(s): Bounds
+# Last Edit: 2022-11-04
+# Author(s): Bounds Hayden
 
 from typing import List
 from uuid import UUID
 from .ProfileManagementServiceInterface import ProfileManagementServiceInterface as PMSI
 from ..Data import *
+import sqlite3
 
 class ProfileManagementService(PMSI):
     def __init__(self):
         # TODO database conn work here. This can be handled by a passed variable during init
         # or just a hard coded string. Either way, its going to be a hard coded value - the user
         # wont get a choice in this storage location.
-        pass
+        self.conn = sqlite3.connect(r".\ImgCrpytDB.db")
+        self.c = self.conn.cursor()
+        #I declared key as int because I wasn't sure what you wanted me to do with UUID generation
+        self.c.execute(""" CREATE TABLE IF NOT EXISTS UserProfiles (
+        Key INTEGER PRIMARY KEY,
+        Name varchar(256) NOT NULL); """)
+        self.c.execute(""" CREATE TABLE IF NOT EXISTS SettingsProfiles (
+        Key INTEGER PRIMARY KEY,
+        Name varchar(256) NOT NULL,
+        CharPerPixel int,
+        PixelSpacing bigint,
+        ColorSettings int,
+        EncryptionKey varchar(256),
+        UserProfile int NOT NULL); """)
 
     # -- User Profiles --
     def create_user_profile(self, name: str) -> UserProfile:
-        return super().create_user_profile(name)
+        return super().create_user_profile(name, self.conn)
 
     def delete_user_profile(self, key: UUID) -> bool:
         return super().delete_user_profile(key)
