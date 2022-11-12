6c8eb8929af5ed7bc83955e96d6e626b80d53dc3
ChopstickLeg <72234304+ChopstickLeg@users.noreply.github.com>
2022-11-11 23:50:35 -0500

    Fully implemented all methods

--- src/Core/Services/ProfileManagementService.py
+++ src/Core/Services/ProfileManagementService.py
@@ -38,38 +38,91 @@ class ProfileManagementService(PMSI):
         #Unsure what to do with the UUID part of creation
         #When using this method, leave out the key part UUID should autogenerate
 
-    def delete_user_profile(self, key: UUID) -> bool:
-        keyTup = (key,)
+    def delete_user_profile(self, key: int) -> bool:
+        keyTup = (key[0],)
         self.c.execute(''' DELETE FROM UserProfiles WHERE Key=?''', keyTup)
 
     def get_all_user_profiles(self) -> List[UserProfile]:
-        self.c.execute('''SELECT * FROM UserProfiles''')
-        return self.c.fetchall()
+        self.c.execute('''SELECT key FROM UserProfiles''')
+        name = ''
+        userProfileList = []
+        for item in self.c.fetchall():
+            self.c.execute("""SELECT name FROM UserProfiles WHERE Key=?""", (item[0],))
+            name = self.c.fetchall()
+            profile = UserProfile(name)
+            profile.uuid = item
+            userProfileList.append(profile)
+        return userProfileList
 
-    def get_settings_profiles_for_user(self, profile: UserProfile) -> None:
-        keyTup = (profile.UUID,)
-        self.c.execute('''SELECT * FROM SettingsProfiles WHERE UserProfile=?''', keyTup)
+
+    def get_settings_profiles_for_user(self, parentKey: int) -> None:
+        #Bug here, for some reason there's apparently a value error on the next line
+        self.c.execute('''SELECT key FROM SettingsProfiles WHERE UserProfile=?''', (int(parentKey[0])))
+        settingsProfileList = []
+        name = ''
+        charPerPixel = 0
+        pixelSpacing = 0
+        colorSettings = 0
+        encryptKey = ''
+        for item in self.c.fetchall():
+            self.c.execute("""SELECT Name FROM SettingsProfiles WHERE Key=?""", (item[0],))
+            name = self.c.fetchall()
+            self.c.execute("""SELECT CharPerPixel FROM SettingsProfiles WHERE Key=?""", (item[0],))
+            charPerPixel = self.c.fetchall()
+            self.c.execute("""SELECT PixelSpacing FROM SettingsProfiles WHERE Key=?""", (item[0],))
+            pixelSpacing = self.c.fetchall()
+            self.c.execute("""SELECT ColorSettings FROM SettingsProfiles WHERE Key=?""", (item[0],))
+            colorSettings = self.c.fetchall()
+            self.c.execute("""SELECT EncryptionKey FROM SettingsProfiles WHERE Key=?""", (item[0],))
+            encryptKey = self.c.fetchall()
+            profile = SettingsProfile(name, charPerPixel, pixelSpacing, colorSettings, encryptKey)
+            profile.uuid = item
+            settingsProfileList.append(profile)
+        return settingsProfileList
 
     # -- -- --
     # -- Settings Profiles --
-    def create_settings_profile(self, name: str, parentKey: UUID) -> SettingsProfile:
-        newProfile = SettingsProfile(name, 0, 0, "", "")
-        #Unsure how you want me to deal with UUID generation
-        values = ('', name, '', '', '', '', parentKey)
-        self.c.execute('''INSERT INTO SettingsProfiles(Key, Name, CharPerPixel, PixelSpacing, ColorSettings, EncryptionKey, UserProfile) VALUES(?,?,?,?,?,?,?)''', values)
+    def create_settings_profile(self, name: str, parentKey: int) -> SettingsProfile:
+        newProfile = SettingsProfile(name, 0, 0, 0, "")
+        values = (name, '', '', '', None, int(parentKey[0]))
+        self.c.execute('''INSERT INTO SettingsProfiles(Name, CharPerPixel, PixelSpacing, ColorSettings, EncryptionKey, UserProfile) VALUES(?,?,?,?,?,?)''', values)
+        self.conn.commit()
 
-    def delete_settings_profile(self, key: UUID) -> bool:
-        keyTup = (key,)
+    def delete_settings_profile(self, key: int) -> bool:
+        keyTup = (key[0],)
         self.c.execute(''' DELETE FROM SettingsProfiles WHERE Key=?''', keyTup)
 
-    def update_settings_profile(updatedProfile: SettingsProfile) -> bool:
-        return super().update_settings_profile()
+    def update_settings_profile(self, updatedProfile: SettingsProfile) -> bool:
+        self.c.execute('''UPDATE SettingsProfiles SET CharPerPixel = ?, PixelSpacing = ?, ColorSettings = ?, EncryptionKey = ? WHERE Key = ?''', (updatedProfile.charPerPixel, updatedProfile.pixelSpacing, updatedProfile.colorSettings, updatedProfile.encryptKey, updatedProfile.uuid))
+        self.conn.commit()
 
     def get_all_settings_profiles(self) -> List[SettingsProfile]:
-        self.c.execute('''SELECT * FROM SettingsProfiles''')
-        return self.c.fetchall()
+        self.c.execute('''SELECT key FROM SettingsProfiles''')
+        settingsProfileList = []
+        name = ''
+        charPerPixel = 0
+        pixelSpacing = 0
+        colorSettings = 0
+        encryptKey = ''
+        for item in self.c.fetchall():
+            self.c.execute("""SELECT Name FROM SettingsProfiles WHERE Key=?""", (item[0],))
+            name = self.c.fetchall()
+            self.c.execute("""SELECT CharPerPixel FROM SettingsProfiles WHERE Key=?""", (item[0],))
+            charPerPixel = self.c.fetchall()
+            self.c.execute("""SELECT PixelSpacing FROM SettingsProfiles WHERE Key=?""", (item[0],))
+            pixelSpacing = self.c.fetchall()
+            self.c.execute("""SELECT ColorSettings FROM SettingsProfiles WHERE Key=?""", (item[0],))
+            colorSettings = self.c.fetchall()
+            self.c.execute("""SELECT EncryptionKey FROM SettingsProfiles WHERE Key=?""", (item[0],))
+            encryptKey = self.c.fetchall()
+            profile = SettingsProfile(name, charPerPixel, pixelSpacing, colorSettings, encryptKey)
+            profile.uuid = item
+            settingsProfileList.append(profile)
+        return settingsProfileList
 
-    def get_settings_profile(self, key: UUID) -> SettingsProfile:
-        return super().get_settings_profile(key)
+
+    def get_settings_profile(self, key: int) -> SettingsProfile:
+        self.c.execute("""SELECT * FROM SettingsProfiles WHERE Key = ?""", (key, ))
+        return self.c.fetchall()
 
     # -- -- --
